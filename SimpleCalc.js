c6572ecca8e6e7a9f380cde5f2d7d8228471504b
Pavlo Charnyi <pavlo.charnyi@gmail.com>
2022-03-29 14:18:28 +0300

    created simplecalc project

---
+++ SimpleCalc.js
@@ -0,0 +1,24 @@
+export class SimpleCalc {
+
+/*     constructor (argOne, argTwo){
+            this.argOne = argOne
+            this.argTwo = argTwo
+    } */
+
+    add(argOne, argTwo)
+    {
+        return argOne + argTwo 
+    }
+    subtract(argOne, argTwo)
+    {
+        return argOne - argTwo 
+    }
+    multiply(argOne, argTwo)
+    {
+        return argOne * argTwo 
+    }
+    divide(argOne, argTwo)
+    {
+        return argOne / argTwo 
+    }
+}
---
+++ calculator.js
@@ -0,0 +1,8 @@
+import { SimpleCalc } from "./SimpleCalc.js";
+
+let calculator = new SimpleCalc()
+
+console.log (calculator.add(16, 4))
+console.log (calculator.subtract(20, 10))
+console.log (calculator.multiply(5, 5))
+console.log (calculator.divide(8, 2))
